40ebb04157555d9125eef5ebf32aef42d7d78518
Leonardo <fanger53@gmail.com>
2021-04-27 15:50:22 -0500

    fuction render added

--- src/index.js
+++ src/index.js
@@ -1,42 +1,33 @@
-import "./style.scss";
-class toDo {
-  constructor(title, des, date, num) {
-    this.fdes = des;
-    this.ftitle = title;
-    this.fdue = date;
-    this.fnum = num;
-  }
+import './style.scss';
+
+const listsContainer = document.querySelector('[data-lists]')
+// class toDo {
+//   constructor(title, des, date, num) {
+//     this.fdes = des;
+//     this.ftitle = title;
+//     this.fdue = date;
+//     this.fnum = num;
+//   }
+// }
+
+let lists =[{
+	id: 1,
+	name: 'name'
+}]
+
+function render()	{
+	clearElement(listsContainer)
+	lists.forEach(list => {
+		const listElement = document.createElement('li')
+		listElement.dataset.listId = list.id
+		listElement.classList.add("list-name")
+		listElement.innerText = list.name
+		listsContainer.appendChild(listElement)
+	})
 }
 
-var toDos = []
-var lol = new toDo("1","1","1","1")
-toDos.push(lol)
-console.log(toDos)
-
-class UI {
-static addToDolist(lol) {
-    	const list = document.querySelector('#list');
-    	const row = document.createElement('tr');
-    	row.innerHTML = `
-      	<td>${toDo.title}</td>
-      	<td>${toDo.des}</td>
-      	<td>${toDo.date}</td>
-	<td>${toDo.num}</td>
-      	<td class="has-text-centered"><button value="Read">Read</button></td>
-      	<td><a href="#" class="btn btn-danger btn-sm delete">Delete</a></td>
-    	`;
-    	list.appendChild(row);
-}
+function clearElement(element) {
+	
 }
 
-document.querySelector('#formToDo').addEventListener('submit', (e) => {
-  	e.preventDefault();
-  	const title = document.querySelector('#ftitle').value;
-  	const des = document.querySelector('#fdes').value;
-  	const date = document.querySelector('#fdue').value;
-  	const num = document.querySelector('#fnum').value;
-	var lol = new toDo(title,des,date,num)
-	toDos.push(lol)
-	console.log(toDos)
-
-})
+render()
